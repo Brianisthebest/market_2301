245d558a12340af95fc50df8e4370f5fe0ac8673
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 09:20:42 -0700

    Add tests for vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -12,6 +12,18 @@ RSpec.describe Vendor do
     it 'exists and has attributes' do
       expect(@vendor).to be_a(Vendor)
       expect(@vendor.name).to eq('Rocky Mountain Fresh')
+      expect(@vendor.inventory).to eq({})
+    end
+
+    it 'can check stock of items and add to inventory' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+      @vendor.stock(@item1, 30)
+      expect(@vendor.inventory).to eq(@item1 => 30)
+      expect(@vendor.check_stock(@item1)).to eq(30)
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(item1)).to eq(55)
+      @vendor.stock(item2, 12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
     end
   end
 end
