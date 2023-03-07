d8dcb57bc310c963e2d2ee9ff1f43e5b1e889bdf
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 10:01:01 -0700

    Add tests for vendor_names method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -20,21 +20,21 @@ RSpec.describe Market do
     expect(@market.vendors).to eq([])
   end
 
-  it 'can add a vendor' do
+  it 'can add a vendor and list names' do
     @market.add_vendor(@vendor1)   
-    expect(@market.vendor).to eq([@vendor1])
+    expect(@market.vendors).to eq([@vendor1])
+    expect(@market.vendor_names).to eq(['Rocks Mountain Fresh'])
     @market.add_vendor(@vendor2) 
     @market.add_vendor(@vendor3) 
-    expect(@market.vendor).to eq([@vendor1, @vendor2, @vendor3])
+    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    expect(@market.vendor_names).to eq(['Rocky Mountain Fresh', 'Ba-Nom-a-Nom', 'Palisade Peach Shack'])
   end
 end
 
-# 1. Market Creation - including all attr_readers
-# 2. Market #add_vendor
 # 3. Market #vendor_names
 # 4. Market #vendors_that_sell
 
-# A Market is responsible for keeping track of Vendors. It should have a method called `vendor_names` that returns an array of all the Vendor's names.
+# It should have a method called `vendor_names` that returns an array of all the Vendor's names.
 
 # Additionally, the Market should have a method called `vendors_that_sell` that takes an argument of an item. It will return a list of Vendors that have that item in stock.
 
