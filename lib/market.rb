85f60183fcc86b741a38519f62b11efed55534e5
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 10:19:06 -0700

    Fix tests and add passing code for vendors that sell method

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,14 @@ class Market
     end
     ven_names
   end
+
+  def vendors_that_sell(item)
+    vendors_with = []
+    @vendors.each do |vendor|
+      if vendor.inventory.include?(item)
+        vendors_with << vendor
+      end
+    end
+    vendors_with
+  end
 end
