7d66dac793068c042c961a1eb879272f177eb479
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 09:24:26 -0700

    Add working code for iteration 1

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,18 @@
 class Vendor
-  
+  attr_reader :name,
+              :inventory,
+              :stock
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    @inventory.fetch(item, 0)
+  end
+
+  def stock(item, amount)
+    @inventory[item] = check_stock(item) + amount
+  end
 end
