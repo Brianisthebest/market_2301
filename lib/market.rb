48024e5c1549ba99d1a556b47ab0ee63256f344c
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 10:03:08 -0700

    Add working code for vendor names method

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,20 @@
 class Market
-  
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors.push(vendor)
+  end
+
+  def vendor_names
+    ven_names = []
+    @vendors.each do |vendor|
+    ven_names.push(vendor.name)
+    end
+    ven_names
+  end
 end
