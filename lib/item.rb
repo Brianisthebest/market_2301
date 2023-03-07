34285e84b90c5acd2ec566081439cad38bc0799e
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-07 09:11:10 -0700

    Add passing code for item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,11 @@
 class Item
-  
+  attr_reader :name, :price
+  def initialize(item_details)
+    @name = item_details[:name]
+    @price = set_price(item_details[:price])
+  end
+
+  def set_price(price)
+    price.delete('$').to_f
+  end
 end
